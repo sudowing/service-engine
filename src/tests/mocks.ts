60c909e49ce0168516efbf584a63a1360c75d0e3
Joe Wingard <accounts@joewingard.com>
2020-05-19 20:22:55 -0400

    Release/v0.0.2 (#3)
    initial release

---
+++ src/tests/mocks.ts
@@ -0,0 +1,320 @@
+import * as Joi from "@hapi/joi";
+
+import * as cnst from "../const";
+
+export const testKeyedTable = Joi.object({
+  alpha: Joi.string().invalid(cnst.SYMBOL_UNIQUE_KEY_COMPONENT),
+  bravo: Joi.number().invalid(cnst.SYMBOL_UNIQUE_KEY_COMPONENT),
+  charlie: Joi.boolean().invalid(cnst.SYMBOL_UNIQUE_KEY_COMPONENT),
+  mike: Joi.string(),
+  xray: Joi.number().invalid(...cnst.SYMBOLS_GEO_POINT),
+  yankee: Joi.number().invalid(...cnst.SYMBOLS_GEO_POLYGON),
+  zulu: Joi.boolean().invalid(cnst.SYMBOL_SOFT_DELETE),
+});
+
+export const testTable = Joi.object({
+  alpha: Joi.string().required(),
+  bravo: Joi.string(),
+  charlie: Joi.number(),
+  delta: Joi.number().integer(),
+  echo: Joi.boolean(),
+  foxtrot: Joi.number(),
+  golf: Joi.string(),
+  hotel: Joi.string().required(),
+  mike: Joi.number().invalid(cnst.SYMBOL_GEOQUERY, cnst.SYMBOL_GEOQUERY_POINT),
+  november: Joi.number().invalid(
+    cnst.SYMBOL_GEOQUERY,
+    cnst.SYMBOL_GEOQUERY_POLYGON
+  ),
+  oscar: Joi.number().invalid(cnst.SYMBOL_GEOQUERY, cnst.SYMBOL_GEOQUERY_POINT),
+  papa: Joi.number().invalid(
+    cnst.SYMBOL_GEOQUERY,
+    cnst.SYMBOL_GEOQUERY_POLYGON
+  ),
+  zulu: Joi.boolean().invalid(cnst.SYMBOL_SOFT_DELETE),
+});
+
+export const initDescribeOriginal = {
+  alpha: {
+    type: "string",
+    required: true,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  bravo: {
+    type: "number",
+    required: true,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  charlie: {
+    type: "boolean",
+    required: true,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  mike: {
+    type: "string",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  xray: {
+    type: "number",
+    required: false,
+    geoqueryType: "point",
+    softDeleteFlag: false,
+  },
+  yankee: {
+    type: "number",
+    required: false,
+    geoqueryType: "polygon",
+    softDeleteFlag: false,
+  },
+  zulu: {
+    type: "boolean",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: true,
+  },
+};
+
+export const initDescribeBizarro = {
+  alpha: {
+    type: "string",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  bravo: {
+    type: "number",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  charlie: {
+    type: "boolean",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  mike: {
+    type: "string",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: false,
+  },
+  xray: {
+    type: "number",
+    required: false,
+    geoqueryType: "point",
+    softDeleteFlag: false,
+  },
+  yankee: {
+    type: "number",
+    required: false,
+    geoqueryType: "polygon",
+    softDeleteFlag: false,
+  },
+  zulu: {
+    type: "boolean",
+    required: false,
+    geoqueryType: null,
+    softDeleteFlag: true,
+  },
+};
+
+export const validationInputs = {
+  input: {
+    number: "123",
+    string: "some_string",
+    boolean: "blah blah blah",
+  },
+  output: {
+    number: 123,
+    string: "some_string",
+    boolean: true,
+  },
+};
+
+export const productConcatErrorMessages = [
+  "alpha 'the_black_keys' argument #1",
+  "bravo 'the_black_keys' argument #3",
+  "charlie 'the_black_keys' argument #5",
+];
+
+export const exampleSearchQuery = {
+  zulu: "true",
+  // field_gt: "field__gt",
+  // field_gte: "field__gte",
+  // field_lt: "field__lt",
+  // field_lte: "field__lte",
+  // field_not: "field__not",
+  // field_range: "field__range",
+  // field_in: "field__in",
+  // field_not_in: "field__not_in",
+  // field_like: "field__like",
+  // field_or: "field__or",
+  // field_geo_bbox: "field__geo_bbox",
+  // field_geo_radius: "field__geo_radius",
+  // field_geo_polygon: "field__geo_polygon",
+  // field_geo_geojson: "field__geo_geojson",
+  "alpha.gt": "field.gt",
+  "bravo.gte": "field.gte",
+  "charlie.lt": "42",
+  "delta.lte": "111",
+  "echo.not": "field.not",
+  "foxtrot.range": "5.1,9.7",
+  "golf.in": "braves,marlins,nationals,mets,phillies",
+  "hotel.not_in": "braves,marlins,nationals,mets,phillies",
+  "alpha.like": "field.like",
+  "mike.geo_bbox": "1.1,2.2,3.3,4.4",
+  "november.geo_radius": "1.2,2.3,111000",
+  // "mike.geo_bbox": "one, two, three, four",
+  // "november.geo_radius": "one, two, three",
+  "alpha.range": "5.1,9.7,9,8,7,6,5",
+  "charlie.range": "5q1,9r7",
+  "alpha.not_a_real_operation": "yada yada yada",
+  not_a_real_prop: "5.1,9.7",
+  "fake_prop.in": "5.1,9.7",
+};
+
+export const exampleParsedSearchQuery = {
+  errors: [
+    {
+      field: "alpha",
+      error: "'range' operation requires 2 args. 7 were provided.",
+    },
+    {
+      field: "charlie",
+      error:
+        "'charlie' argument #0 must be a number,'charlie' argument #1 must be a number",
+    },
+    {
+      field: "alpha",
+      error: "'not_a_real_operation' operation not supported",
+    },
+    {
+      field: "not_a_real_prop",
+      error: "'not_a_real_prop' is not a supported property on this resource",
+    },
+  ],
+  components: [
+    {
+      field: "zulu",
+      rawValue: "true",
+      operation: "equal",
+      type: "boolean",
+      value: true,
+    },
+    {
+      field: "alpha",
+      rawValue: "field.gt",
+      operation: "gt",
+      type: "string",
+      value: "field.gt",
+    },
+    {
+      field: "bravo",
+      rawValue: "field.gte",
+      operation: "gte",
+      type: "string",
+      value: "field.gte",
+    },
+    {
+      field: "charlie",
+      rawValue: "42",
+      operation: "lt",
+      type: "number",
+      value: 42,
+    },
+    {
+      field: "delta",
+      rawValue: "111",
+      operation: "lte",
+      type: "number",
+      value: 111,
+    },
+    {
+      field: "echo",
+      rawValue: "field.not",
+      operation: "not",
+      type: "boolean",
+      value: true,
+    },
+    {
+      field: "foxtrot",
+      rawValue: "5.1,9.7",
+      operation: "range",
+      type: "number",
+      value: [5.1, 9.7],
+    },
+    {
+      field: "golf",
+      rawValue: "braves,marlins,nationals,mets,phillies",
+      operation: "in",
+      type: "string",
+      value: ["braves", "marlins", "nationals", "mets", "phillies"],
+    },
+    {
+      field: "hotel",
+      rawValue: "braves,marlins,nationals,mets,phillies",
+      operation: "not_in",
+      type: "string",
+      value: ["braves", "marlins", "nationals", "mets", "phillies"],
+    },
+    {
+      field: "alpha",
+      rawValue: "field.like",
+      operation: "like",
+      type: "string",
+      value: "field.like",
+    },
+    {
+      field: "mike",
+      rawValue: "1.1,2.2,3.3,4.4",
+      operation: "geo_bbox",
+      type: "number",
+      value: [1.1, 2.2, 3.3, 4.4],
+    },
+    {
+      field: "november",
+      rawValue: "1.2,2.3,111000",
+      operation: "geo_radius",
+      type: "number",
+      value: [1.2, 2.3, 111000],
+    },
+    {
+      field: "alpha",
+      rawValue: "5.1,9.7,9,8,7,6,5",
+      operation: "range",
+      type: "string",
+      value: ["5.1", "9.7", "9", "8", "7", "6", "5"],
+    },
+    {
+      field: "charlie",
+      rawValue: "5q1,9r7",
+      operation: "range",
+      type: "number",
+      value: [null, null],
+    },
+    {
+      field: "alpha",
+      rawValue: "yada yada yada",
+      operation: "not_a_real_operation",
+      type: "string",
+      value: "yada yada yada",
+    },
+    {
+      field: "not_a_real_prop",
+      rawValue: "5.1,9.7",
+      operation: "equal",
+    },
+    {
+      field: "fake_prop",
+      rawValue: "5.1,9.7",
+      operation: "in",
+      value: ["5.1", "9.7"],
+    },
+  ],
+};
---
+++ src/utils.ts
@@ -0,0 +1,477 @@
+import * as Joi from "@hapi/joi";
+
+import { cloneDeep } from "lodash";
+import * as cnst from "./const";
+import * as ts from "./interfaces";
+
+export const castString = (arg) => String(arg);
+export const castNumber = (arg) => Number(arg);
+export const castBoolean = (arg) =>
+  Boolean(cnst.FALSEY_STRING_VALUES.includes(arg) ? false : arg);
+export const castOther = (arg) => arg;
+
+/**
+ * @description Fn that takes string input of a data type, and returns a Fn that will convert an input into target type. Boolean defines some string inputs as falesy and returns false.
+ * @param {string} type
+ * @returns {(ts.ITypeCastString
+ *   | ts.ITypeCastNumber
+ *   | ts.ITypeCastBoolean
+ *   | ts.IDefaultTypeCast
+ *   | any)}
+ */
+export const typecastFn = (
+  type: string
+):
+  | ts.ITypeCastString
+  | ts.ITypeCastNumber
+  | ts.ITypeCastBoolean
+  | ts.IDefaultTypeCast
+  | any => {
+  switch (type) {
+    case cnst.STRING:
+      return castString;
+    case cnst.NUMBER:
+      return castNumber;
+    case cnst.BOOLEAN:
+      return castBoolean;
+    default:
+      return castOther;
+  }
+};
+
+/**
+ * @description Fn that takes a JOI validator as input and modifies fields (making required or totally removing) fields that are marked with appropriate SYMBOLS. This is useful so that validators that define records 1:1 with db resource DDLs (table, view, materialized view), dynamically get modified to support full CRUD operations.
+ * @param {Joi.Schema} validator
+ * @param {string} [operation='original']
+ * @returns {Joi.Schema}
+ */
+export const modifyValidator = (
+  validator: Joi.Schema,
+  operation: string = "original"
+): Joi.Schema => {
+  const newValidator = cloneDeep(validator);
+  newValidator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].forEach(
+    ({ schema, id }) => {
+      if (operation === cnst.CREATE) {
+        if (
+          schema._invalids &&
+          schema._invalids.has(cnst.SYMBOL_CREATE_DISABLED)
+        ) {
+          newValidator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].delete(id);
+        }
+        if (
+          schema._invalids &&
+          schema._invalids.has(cnst.SYMBOL_CREATE_REQUIRED)
+        ) {
+          schema._flags = schema._flags
+            ? { ...schema._flags, ...cnst.REQUIRED_FLAG }
+            : cnst.REQUIRED_FLAG;
+        }
+      } else if (operation === cnst.UPDATE) {
+        // required all pk fields for update
+        if (
+          schema._invalids &&
+          schema._invalids.has(cnst.SYMBOL_UNIQUE_KEY_COMPONENT)
+        ) {
+          schema._flags = schema._flags
+            ? { ...schema._flags, ...cnst.REQUIRED_FLAG }
+            : cnst.REQUIRED_FLAG;
+        }
+
+        // blacklist immutable fields (future feature to set via config)
+        if (
+          schema._invalids &&
+          schema._invalids.has(cnst.SYMBOL_UPDATE_DISABLED)
+        ) {
+          newValidator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].delete(id);
+        }
+      } else if (operation === cnst.READ) {
+        if (
+          schema._invalids &&
+          schema._invalids.has(cnst.SYMBOL_UNIQUE_KEY_COMPONENT)
+        ) {
+          schema._flags = schema._flags
+            ? { ...schema._flags, ...cnst.REQUIRED_FLAG }
+            : cnst.REQUIRED_FLAG;
+        }
+      } else if (operation === cnst.DELETE) {
+        if (
+          schema._invalids &&
+          schema._invalids.has(cnst.SYMBOL_UNIQUE_KEY_COMPONENT)
+        ) {
+          schema._flags = schema._flags
+            ? { ...schema._flags, ...cnst.REQUIRED_FLAG }
+            : cnst.REQUIRED_FLAG;
+        } else {
+          newValidator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].delete(id);
+        }
+      }
+    }
+  );
+  return newValidator;
+};
+
+export const validate = {
+  create: (validator: Joi.Schema): Joi.Schema =>
+    modifyValidator(validator, cnst.CREATE),
+  read: (validator: Joi.Schema): Joi.Schema =>
+    modifyValidator(validator, cnst.READ),
+  update: (validator: Joi.Schema): Joi.Schema =>
+    modifyValidator(validator, cnst.UPDATE),
+  delete: (validator: Joi.Schema): Joi.Schema =>
+    modifyValidator(validator, cnst.READ),
+  search: (validator: Joi.Schema): Joi.Schema => validator,
+};
+
+/**
+ * @description Reducer Fn that inspects a given schema from fields/keys within a JOI validator object and reports meta information about the specific field. This information is used extensively in this framework for autogeneration of openapi docs, pks to use for unique records, dynamic enabling of geoquery interfaces, and typecasting of query string input before building SQL statements.
+ * @param {object} accum
+ * @param {*} { id, schema }
+ * @returns {ts.IValidatorInspectorReport}
+ */
+export const reducerValidatorInspector = (
+  accum: object,
+  { id, schema }
+): ts.IValidatorInspectorReport => ({
+  ...accum,
+  [id]: {
+    type: schema.type,
+    required: !!(schema._flags && schema._flags.presence),
+    keyComponent: !!(
+      schema._invalids && schema._invalids.has(cnst.SYMBOL_UNIQUE_KEY_COMPONENT)
+    ),
+    geoqueryType:
+      schema._invalids && schema._invalids.has(cnst.SYMBOL_GEOQUERY)
+        ? schema._invalids.has(cnst.SYMBOL_GEOQUERY_POINT)
+          ? cnst.POINT
+          : cnst.POLYGON
+        : null,
+    softDeleteFlag: !!(
+      schema._invalids && schema._invalids.has(cnst.SYMBOL_SOFT_DELETE)
+    ),
+    updateDisabled: !!(
+      schema._invalids && schema._invalids.has(cnst.SYMBOL_UPDATE_DISABLED)
+    ),
+    createRequired: !!(
+      schema._invalids && schema._invalids.has(cnst.SYMBOL_CREATE_REQUIRED)
+    ),
+    createDisabled: !!(
+      schema._invalids && schema._invalids.has(cnst.SYMBOL_CREATE_DISABLED)
+    ),
+    typecast: typecastFn(schema.type), // prob need dynamaic assignment for geo fields (need input as numbers and strings?)
+    validate: (value: string) =>
+      schema.validate((typecastFn(schema.type) as any)(value)), // this needs to be async validation
+  },
+});
+
+/**
+ * @description Used for swagger generation and for validating user queries. Real validator cannot be used as those are plain objects and may need to validate field multiple times (sql where field <= 5 and >= 12) <-- need to validate values against field twice.
+ * @param {Joi.Schema} validator
+ * @returns {ts.IValidatorInspectorReport}
+ */
+export const validatorInspector = (
+  validator: Joi.Schema
+): ts.IValidatorInspectorReport =>
+  (Array.from(
+    validator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].values()
+  ) as any).reduce(reducerValidatorInspector, {});
+
+/**
+ * @description Fn that generates error message when value submitted for search is of incorrect data type.
+ * @param {Error} error
+ * @param {string} field
+ * @returns {string}
+ */
+export const errorMessageInvalidValue = (error: Error, field: string): string =>
+  error.message.replace(cnst.QUOTED_VALUE, `'${field}'`);
+
+/**
+ * @description Fn that generates error message when GET query produces error for various reasons. Defined errors were generated via JOI validation checks. undefined types occur when trying to query a column that doesn't exist on the record. Final fallback is for when a query operation is called on a field that is not supported.
+ * @param {ts.IParamsGenerateSearchQueryError} {error, field, type, operation}
+ * @returns {string}
+ */
+export const generateSearchQueryError = ({
+  error,
+  field,
+  type,
+  operation,
+}: ts.IParamsGenerateSearchQueryError): string =>
+  error
+    ? errorMessageInvalidValue(error, field)
+    : !type
+    ? `'${field}' is not a supported property on this resource`
+    : `'${operation}' operation not supported`;
+
+/**
+ * @description Fn that generates verbose error message string when specific number of args not used with a given operation. Only a few operations have specific arg lengths.
+ * @param {string} operation
+ * @param {any[]} values
+ * @returns {string}
+ */
+export const badArgsLengthError = (operation: string, values: any[]): string =>
+  `'${operation}' operation requires ${cnst.DEFINED_ARG_LENGTHS[operation]} args. ${values.length} were provided.`;
+
+/**
+ * @description Convenience Fn that builds array of verbose error message strings
+ * @param {string} field
+ */
+export const concatErrorMessages = (field: string) => (
+  accum,
+  { error },
+  i
+): string[] => [
+  ...accum,
+  ...(error
+    ? [error.message.replace(cnst.QUOTED_VALUE, `'${field}' argument #${i}`)]
+    : []),
+];
+
+/**
+ * @description Fn that checks to see if length of args meets the needs for a given operation. Only applies to a handful of operations, so majority default to true.
+ * @param {string} operation
+ * @param {any[]} values
+ * @returns {boolean}
+ */
+export const validArgsforOperation = (
+  operation: string,
+  values: any[]
+): boolean =>
+  cnst.DEFINED_ARG_LENGTHS[operation]
+    ? cnst.DEFINED_ARG_LENGTHS[operation] === values.length
+    : true;
+
+/**
+ * @description Some operations on GET queries support operations via `field.operation`. A subset of these operations support multiple values seperated by commas. This `SUPPORTED_OPERATIONS` object holds a map with keys that represent all supported operations and values that define the support for multiple values. `undefined` responses to this function only occur of the operation isn't supported on the field at all.
+ * @param {string} operation
+ * @returns {(boolean)}
+ */
+export const supportMultipleValues = (operation: string): boolean =>
+  !!cnst.SUPPORTED_OPERATIONS[operation];
+
+/**
+ * @description Some operations on GET queries support operations via `field.operation`. This `SUPPORTED_OPERATIONS` object holds a map with keys that represent all supported operations.
+ * @param {string} operation
+ * @returns {boolean}
+ */
+export const supportedOperation = (operation: string): boolean =>
+  cnst.SUPPORTED_OPERATIONS.hasOwnProperty(operation);
+
+/**
+ * @description Fn that parses all GET querystring keys into fields and operations. These are set by the caller via `field.operation` syntax on all GET query string keys for searching. If `.operation` is not provided -- defaults to `.equal`.
+ * @param {string} key
+ * @returns {ts.IFieldAndOperation}
+ */
+export const parseFieldAndOperation = (key: string): ts.IFieldAndOperation => {
+  const [field, op] = key.split(cnst.DOT);
+  return {
+    field,
+    operation: op ? op : cnst.EQUAL,
+  };
+};
+
+const contextTransformer = (attribute, input) => {
+  switch (attribute) {
+    case cnst.FIELDS:
+      return input.split(cnst.COMMA);
+    case cnst.ORDERBY:
+      return input.split(cnst.COMMA).map((fieldAndDirection) => {
+        const [column, direction] = fieldAndDirection.split(cnst.COLON);
+        return {
+          column,
+          order: direction && cnst.DESC === direction ? direction : cnst.ASC,
+        };
+      });
+    case cnst.PAGE:
+    case cnst.LIMIT:
+      return Number(input);
+    case cnst.NOTWHERE:
+      return !castBoolean(input); // castBoolean returns bool based on falsey input. I want to default to the opposite.
+    default:
+      return false;
+  }
+};
+
+/**
+ * @description Heart of the entire system. This Fn takes in a JOI validator and a query object (`ctx.request.query`) and submits both for processing. Search interfaces, fields & operations, are derived from the JOI validators, values from query object are typecasted to data types (if possible) using the types of each field from the JOI validator. Some query operations support multiple values seperated by commas, and this value parsing to arrays is also done here. The `context` object returned is used to build the query -- but is more superficial than the `components` which are used within the `where` part of the query. Any errors with a give query against a validator produces an array of errors, so the request can be stopped before submitting to database.
+ * @param {Joi.Schema} validator
+ * @param {ts.IParamsSearchQueryParser} query
+ * @returns {ts.ISearchQueryResponse}
+ */
+export const searchQueryParser = (
+  validator: Joi.Schema,
+  query: ts.IParamsSearchQueryParser,
+  seperator?: string
+): ts.ISearchQueryResponse => {
+  const errors = [];
+  const components = [];
+  const sep = seperator || cnst.SEARCH_QUERY_CONTEXT.seperator;
+
+  // removing context
+  Object.entries(query).forEach(([key, rawValue]) => {
+    const { field, operation } = parseFieldAndOperation(key);
+    const { schema } =
+      validator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].get(field) || {};
+    const { type } = schema || {}; // all fields have types. if undefined -- simply not a field on the resource
+    const record = { field, rawValue, operation, type };
+    const typecast: any = typecastFn(type);
+    if (supportMultipleValues(operation)) {
+      const values = rawValue.split(sep).map(typecast);
+      if (!validArgsforOperation(operation, values))
+        errors.push({ field, error: badArgsLengthError(operation, values) });
+      const validatedValues = schema
+        ? values.map((value) => schema.validate(value))
+        : [];
+      const error = validatedValues
+        .reduce(concatErrorMessages(field), [])
+        .join(cnst.COMMA);
+      if (error) errors.push({ field, error });
+      components.push({ ...record, value: values });
+    } else {
+      const { value, error } = schema
+        ? schema.validate(typecast(rawValue))
+        : ({} as any);
+      if (error || !type || !supportedOperation(operation)) {
+        errors.push({
+          field,
+          error: generateSearchQueryError({ error, field, type, operation }),
+        });
+      }
+      components.push({ ...record, value });
+    }
+  });
+  return { errors, components };
+};
+
+export const queryContextParser = (
+  validator: Joi.Schema,
+  query: ts.IParamsSearchQueryParser
+): ts.IQueryContextResponse => {
+  const errors = [];
+  const context: ts.ISearchQueryContext = {
+    ...cnst.SEARCH_QUERY_CONTEXT,
+    ...(query[cnst.SEPERATOR] ? { seperator: query[cnst.SEPERATOR] } : {}), // support user provided seperators for fields that support multiple values
+  };
+
+  Object.entries(query).forEach(([key, rawValue]) => {
+    if (context.hasOwnProperty(key)) {
+      const value = contextTransformer(key, rawValue);
+      // add order by and fields values to set to ensure they're all part of the validator
+      if (["fields", "orderBy"].includes(key)) {
+        // orderBy value is an array of obects. need to map to get the field names
+        const values =
+          key === "fields" ? value : value.map(({ column }) => column);
+        const unsupportedFields = values.filter(
+          (field) =>
+            !validator[cnst.UNDERSCORE_IDS][cnst.UNDERSCORE_BYKEY].get(field)
+        );
+        // if attempting to use unsupported fields in context -- add error objects
+        if (unsupportedFields.length) {
+          const error = `'${key}' in context does not support submitted fields: ${unsupportedFields.join(
+            ", "
+          )}`;
+          errors.push({ field: key, error });
+        } else {
+          context[key] = value;
+        }
+      } else {
+        context[key] = value;
+      }
+    }
+  });
+  return { errors, context };
+};
+
+// this is to much dupe. can abstract and pass key.path to map
+// this is to much dupe. can abstract and pass key.path to map
+
+/**
+ * @description Accepts Validator Inspection Report as input and lists all softDeleteFields (designed to only support one)
+ * @param {ts.IValidatorInspectorReport} report
+ */
+export const softDeleteFields = (report: ts.IValidatorInspectorReport) =>
+  Object.entries(report).reduce(
+    (accum, [key, { softDeleteFlag }]) =>
+      !softDeleteFlag ? accum : [...accum, key],
+    []
+  );
+
+/**
+ * @description Accepts Validator Inspection Report as input and lists all fields that are components of primary key (designed to only support one)
+ * @param {ts.IValidatorInspectorReport} report
+ */
+export const uniqueKeyComponents = (report: ts.IValidatorInspectorReport) =>
+  Object.entries(report).reduce(
+    (accum, [key, { required }]) => (!required ? accum : [...accum, key]),
+    []
+  );
+
+// :TODO more accurate here
+// http://www.movable-type.co.uk/scripts/latlong.html
+
+export const convertMetersToDecimalDegrees = (meters: number) =>
+  meters / cnst.DD_BASE;
+
+export const validationExpander = (
+  validator: Joi.Schema
+): ts.IValidationExpander => {
+  const schema: ts.IValidationExpanderSchema = {
+    create: modifyValidator(validator, cnst.CREATE),
+    read: modifyValidator(validator, cnst.READ),
+    update: modifyValidator(validator, cnst.UPDATE),
+    delete: modifyValidator(validator, cnst.DELETE),
+    search: modifyValidator(validator, cnst.SEARCH),
+  };
+  const report: ts.IValidationExpanderReport = {
+    create: validatorInspector(schema.create),
+    read: validatorInspector(schema.read),
+    update: validatorInspector(schema.update),
+    delete: validatorInspector(schema.delete),
+    search: validatorInspector(schema.search),
+  };
+  const meta: ts.IValidationExpanderMeta = {
+    softDeleteFields: softDeleteFields(report.read),
+    uniqueKeyComponents: uniqueKeyComponents(report.read),
+    searchQueryParser: (query, context) =>
+      searchQueryParser(
+        validator,
+        query,
+        context && context.sep ? context.sep : undefined
+      ),
+  };
+
+  return { schema, report, meta };
+};
+
+// need to remove context keys
+export const validateOneOrMany = (
+  validator: Joi.Schema,
+  payload: any | any[]
+) =>
+  (Array.isArray(payload) ? Joi.array().items(validator) : validator).validate(
+    payload
+  );
+
+export const removeContextKeys = (context, payload) => {
+  const contextFreePayload = { ...payload };
+  Object.keys(context).forEach((item) => {
+    delete contextFreePayload[item];
+  });
+  return contextFreePayload;
+};
+
+export const rejectResource = (
+  errorType: string,
+  error
+): ts.IRejectResource => ({
+  errorType,
+  error,
+});
+export const resolveResource = (result): ts.IResolveResource => ({ result });
+
+export const nameRestEndpointGetRecords = (
+  resource: string,
+  prefix: string = "service"
+) => ({
+  resourceEndpoint: `/${prefix}/${resource}`,
+  uniqueEndpoint: `/${prefix}/${resource}/record`,
+});
