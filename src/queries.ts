581369b107c7a813db473a366ac1b6a8b664ea5c
Joe Wingard <accounts@joewingard.com>
2020-05-29 23:59:47 -0400

    wip

--- src/queries.ts
+++ src/queries.ts
@@ -83,34 +83,137 @@ export const joiKeyComponent = (joi: Joi.Schema, keyComponent: boolean) =>
 export const joiRequired = (joi: Joi.Schema, required: boolean) =>
   required ? joi : joi; // need to eval .required() here... think it's breaking the framework
 
+  // this is for postgres. Need one for each engine || each engine version
 export const joiBase = (type: string) => {
   switch (type) {
-    case "boolean":
-      return Joi.boolean();
-    case "character":
-    case "character varying":
-    case "character varying(255)":
-    case "text":
-    case "name":
-    case "smallint[]":
-    case "timestamp without time zone":
-    case "timestamp with time zone":
-      return Joi.string();
-    case "integer":
+    // 8.1. Numeric Types":
     case "smallint":
-      return Joi.number().integer();
-    case "double precision":
+    case "integer":
+    case "bigint":
+        return Joi.number().integer();
+    case "decimal":
     case "numeric":
-      return Joi.number();
+    case "real":
+    case "double precision":
+        return Joi.number(); // have special values (Infinity, -Infinity, NaN)
+    case "smallserial":
+    case "serial":
+    case "bigserial":
+        return Joi.number().integer();
+    case "int2":
+    case "int4":
+    case "int8":
+        return Joi.number().integer();
+    // 8.2. Monetary Types":
+    case "money || bigint in js":
+        return Joi.string(); // string as it is arbitrary length
+    // 8.3. Character Types":
+    // case "character varying(n)": // ignore. default will be string
+    // case "varchar(n)": // ignore. default will be string
+    // case "character(n)": // ignore. default will be string
+    // case "char(n)": // ignore. default will be string
+    case "character varying":
+    case "text":
+    case '"char"':
+    case "name":
+        return Joi.string();
+    // 8.4. Binary Data Types":
+    case "bytea":
+        return Joi.string();
+    // 8.5. Date/Time Types":
+    // case "timestamp": tz optional // ignore. default will be string
+    // case "timestamp": wtz // ignore. default will be string
+    case "timestamp without time zone":
+    case "date":
+    // case "time": tz optional // ignore. default will be string
+    // case "time": wtz // ignore. default will be string
+    case "interval":
+        return Joi.string();
+    // 8.6. Boolean Type":
+    case "boolean":
+        return Joi.boolean();
+    // 8.7. Enumerated Types":
+      // ignore. default will be string
+    // 8.8. Geometric Types":
+    case "point":
+    case "line":
+    case "lseg":
+    case "box":
+    case "path":
+    case "path":
+    case "polygon":
+    case "circle":
+        return Joi.string(); // will want geoJson on output
+    // 8.9. Network Address Types":
+    case "cidr":
+    case "inet":
+    case "macaddr":
+        return Joi.string();
+    // 8.10. Bit String Types":
+    // case "bit(n)": // ignore. default will be string
+    // case "bit varying(n)": // ignore. default will be string
+        return Joi.string();
+
+    // 8.11. Text Search Types":
+    // 8.11.1. tsvector":
+    // 8.11.2. tsquery":
+
+    // 8.12. UUID Type":
     case "uuid":
-      return Joi.string().guid();
+    case "string":
+        return Joi.string();
+    // 8.13. XML Type":
+    case "xml":
+        return Joi.string();
+    // 8.14. JSON Types":
+    case "json":
+    case "jsonb":
+    case "jsonpath":
+        return Joi.string(); // will want to use JSONB on output
+
+    // 8.15. Arrays":
+      // ignore. default will be string
+      // in the future -- breaking change will type
+
+    // 8.16. Composite Types":
+      // ignore. default will be string
+
+    // 8.17. Range Types":
+    case "int4range":
+    case "int8range":
+        return Joi.number().integer();
+    case "numrange":
+    case "* float":
+        return Joi.number();
+    case "tsrange":
+    case "tstzrange":
+    case "daterange":
+        return Joi.string();
+    // 8.18. Domain Types": // ignore. let default catch it
+    // 8.19. Object Identifier Types":
+    case "oid":
+        return Joi.number().integer();
+    case "regproc":
+    case "regprocedure":
+    case "regoper":
+    case "regoperator":
+    case "regclass":
+    case "regtype":
+    case "regrole":
+    case "regnamespace":
+    case "regconfig":
+    case "regdictionary":
+        return Joi.string();
+    // 8.20. pg_lsn Type":
+    case "pg_lsn":
+        return Joi.string();
     default:
       const match = type.match(REGEX_CHAR);
       if (match) {
         return Joi.string().length(Number(match.groups.len));
       }
-
-      throw new Error(`unknown type ${type}`);
+      console.log(`unknown type ${type}`);
+      return Joi.string();
   }
 };
 
