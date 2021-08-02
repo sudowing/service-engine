ca29ca831e7387e2b6e7c0f3fbeb5d25eb710ca4
Joe Wingard <open-source@joewingard.com>
2021-08-02 10:18:15 -0400

    reduce dialect mapping based on client

--- src/dialects/index.ts
+++ src/dialects/index.ts
@@ -9,25 +9,22 @@ import { dialect as mssql } from "./mssql";
 export const getDatabaseResources = ({ db }: ts.IDatabaseBootstrap) => {
   const migrationTable: string = db.client.config.migrations.tableName || "";
 
-  if (db.client.config.client === "pg") {
-    return postgres({ migrationTable });
-  }
+  const loadDialectConfig = (fn) => ({
+    ...fn({ migrationTable }),
+    dbGeometryColumns: undefined,
+  });
 
-  if (db.client.config.client === "redshift") {
-    return { ...redshift({ migrationTable }), dbGeometryColumns: undefined };
-  }
+  const supportedClients = {
+    pg: postgres({ migrationTable }),
+    redshift: loadDialectConfig(redshift),
+    sqlite3: loadDialectConfig(sqlite3),
+    mysql: loadDialectConfig(mysql),
+    mysql2: loadDialectConfig(mysql),
+    tedious: loadDialectConfig(mssql),
+  };
 
-  if (db.client.config.client === "sqlite3") {
-    return { ...sqlite3({ migrationTable }), dbGeometryColumns: undefined };
-  }
-
-  // support mysql && mysql2
-  if (db.client.config.client.includes("mysql")) {
-    return { ...mysql({ migrationTable }), dbGeometryColumns: undefined };
-  }
-
-  if (db.client.config.client === "tedious") {
-    return { ...mssql({ migrationTable }), dbGeometryColumns: undefined };
+  if (supportedClients.hasOwnProperty(db.client.config.client)) {
+    return supportedClients[db.client.config.client];
   }
 
   throw new Error("unsupported db engine: postgres, mysql & sqlite3 only");
